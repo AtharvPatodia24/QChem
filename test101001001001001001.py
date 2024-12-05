964b65bf4569c940f6c39435e23b1930a29644b0
Atharv Patodia <atcodinggaming@gmail.com>
2024-12-05 12:41:06 +0530

    Put local trial in Test and used local simulator in main by using COBYLA instead of SLSQP

--- test101001001001001001.py
+++ test101001001001001001.py
@@ -7,15 +7,15 @@ from qiskit_nature.second_q.circuit.library import HartreeFock, UCCSD
 from matplotlib import pyplot as plt
 import psi4
 from qiskit.primitives import Estimator
-from qiskit_algorithms.optimizers import SLSQP
+from qiskit_algorithms.optimizers import SLSQP, COBYLA
 from qiskit_nature.second_q.mappers import JordanWignerMapper
 from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit_nature.second_q.algorithms import GroundStateEigensolver
 from qiskit_nature import settings
 settings.use_ipython = False
 energies = {}
-service = QiskitRuntimeService(channel="ibm_quantum", token="82f6eeb5a67e68bc27f19c170479d281103dddfc005292906679d90a8971f33b84ce8c51ce991c90181edd9734ecc5e9519afc001f203c9447e8247923124966")
-backend = service.least_busy(operational=True, simulator=False)
+#service = QiskitRuntimeService(channel="ibm_quantum", token="IBMQ Token Here")
+#backend = service.least_busy(operational=True, simulator=False)
 molecule_geometry = f"""
 0 1
 Li 0.0 0.0 0.5
@@ -55,7 +55,7 @@ ansatz = UCCSD(
     ),
 )
 
-vqe_solver = VQE(Estimator( ), ansatz, SLSQP())
+vqe_solver = VQE(Estimator( ), ansatz, COBYLA(maxiter=10))
 vqe_solver.initial_point = [0.0] * ansatz.num_parameters
 calc = GroundStateEigensolver(mapper, vqe_solver)
 res = calc.solve(es_problem)
